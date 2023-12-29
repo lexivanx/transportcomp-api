1e4de71b72453316cec1db5e5bfdb4b54737a909
Alexander Ivanov <alexander.ivov.ivanov@proton.me>
2023-12-29 10:56:15 +0200

    Initial commit

---
+++ src/main/java/com/transportcompany/transportcompapi/TransportcompApiApplication.java
@@ -0,0 +1,13 @@
+package com.transportcompany.transportcompapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransportcompApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportcompApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transportcompany/transportcompapi/TransportcompApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transportcompany.transportcompapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportcompApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
