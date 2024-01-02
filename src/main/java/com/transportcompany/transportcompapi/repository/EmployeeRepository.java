6bf08f762775b194c5c2a32dc3eed093fe845698
Alexander Ivanov <alexander.ivov.ivanov@proton.me>
2024-01-02 18:49:22 +0200

    Implement sorting, repositories

--- src/main/java/com/transportcompany/transportcompapi/repository/EmployeeRepository.java
+++ src/main/java/com/transportcompany/transportcompapi/repository/EmployeeRepository.java
@@ -1,8 +1,13 @@
 package com.transportcompany.transportcompapi.repository;
 
 import com.transportcompany.transportcompapi.model.Employee;
+import com.transportcompany.transportcompapi.model.LicenseType;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
+    List<Employee> findByLicenseAndAllowedSpecialCargoAndSalary(LicenseType license, boolean allowedSpecialCargo, BigDecimal salary);
 }
--- src/main/java/com/transportcompany/transportcompapi/repository/ShipmentRepository.java
+++ src/main/java/com/transportcompany/transportcompapi/repository/ShipmentRepository.java
@@ -2,7 +2,19 @@ package com.transportcompany.transportcompapi.repository;
 
 import com.transportcompany.transportcompapi.model.Shipment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
+import java.util.Map;
 
 public interface ShipmentRepository extends JpaRepository<Shipment, Integer> {
 
+    @Query("SELECT new map(c.companyID as companyId, c.name as companyName, SUM(s.price) as revenue) " +
+            "FROM Shipment s " +
+            "JOIN s.driver e " +
+            "JOIN e.company c " +
+            "GROUP BY c.companyID, c.name")
+    List<Map<String, Object>> findCompaniesAndRevenue();
+
+    List<Shipment> findByCustomCriteria(
+            String country, String cityVillageName, String streetName, Integer streetNumber, String entrance);
 }
